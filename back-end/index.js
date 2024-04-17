6a68febdd2f63a259dd274d7c1cbb04bd5c7e063
azeem30 <pinjariashpak3@gmail.com>
2024-04-17 16:46:50 +0530

    CORS middleware

--- back-end/index.js
+++ back-end/index.js
@@ -7,7 +7,8 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "https://frontend-alpha-eight-79.vercel.app");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
